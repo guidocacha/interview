2e709f388163e1071ad45e803a0588a4ad466f59
Guido Cacciatore <guidocacha@hotmail.com>
2022-11-06 20:23:22 -0300

    agrego botón para cambiar gift y frase

--- src/components/callApiCats.jsx
+++ src/components/callApiCats.jsx
@@ -6,6 +6,7 @@ const CallApiCats = () => {
 
     const [fact, setFact] = useState();
     const [gif, setGif] = useState();
+    const [change, setChange] = useState();
 
     useEffect(() => {
         fetch("https://catfact.ninja/fact")
@@ -16,15 +17,23 @@ const CallApiCats = () => {
                     .then(res => res.json())
                     .then(json => setGif(json.data[0].images.original.url))
             })
-    }, []);
+    }, [change]);
 
+    const mouse = e => {
+        setChange(change + e.target.value);
+        console.log("click");
+    }
 
     return (
-        <div style={{ display: "flex", justifyContent: "center", gap: "1rem", alignItems: "center", height: "100vh" }}>
-            <img src={gif} alt={`gif de ${gif}`} style={{ width: "30%" }} />
-            <p style={{ width: "40%" }}>
-                {fact}
-            </p>
+        <div>
+            <div style={{ display: "flex", justifyContent: "center", gap: "1rem", alignItems: "center", height: "70vh" }}>
+                <img src={gif} alt={`gif de ${gif}`} style={{ width: "30%", borderRadius: "1rem" }} />
+                <p style={{ width: "40%" }}>
+                    {fact}
+                </p>
+            </div>
+            <button value= "1" onClick= {mouse} style={{ margin: "3rem", backgroundColor: "black", color: "wheat", padding: "1rem", borderRadius: "1rem", fontSize: "1rem" }}>
+                    cambiar frase y gift</button>
 
         </div>
     )
